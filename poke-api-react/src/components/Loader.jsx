26d84dec2d4dbb8cc90b6733436efb6a8c986038
RicardoC23 <ricardomamani403@gmail.com>
2023-07-04 20:56:36 -0500

    poke api

---
+++ poke-api-react/src/components/Loader.jsx
@@ -0,0 +1,14 @@
+import { DotSpinner } from '@uiball/loaders'
+
+
+export const Loader = () => {
+    return (
+        <div className="container-loader">
+        <DotSpinner 
+            size={40}
+            speed={0.9} 
+            color="black" 
+        />
+        </div>
+    )
+};
